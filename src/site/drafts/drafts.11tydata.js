82111213fe1fc704056dcb4ad3ce2d5723d8d979
David Chen <cgl.david@gmail.com>
2020-05-31 00:40:31 -0700

    Don't copy drafts to production

--- src/site/drafts/drafts.11tydata.js
+++ src/site/drafts/drafts.11tydata.js
@@ -1,12 +1,19 @@
+const path = require('path');
 const slugify = require('slugify');
+
+const isIndex = (page) => path.basename(page.inputPath) === 'index.liquid';
 
 module.exports = {
   eleventyComputed: {
-    layout: 'post',
+    layout: (data) => (isIndex(data.page) ? 'base' : 'post'),
+
     permalink: (data) => {
       if (process.env.ELEVENTY_ENV === 'production') {
-        // Don't copy drafts to the output folder in production build.
+        // Set `permalink: false` to prevent this page from getting copied to
+        // the output folder in production.
         return false;
+      } else if (isIndex(data.page)) {
+        return `drafts/`;
       } else {
         return `drafts/${slugify(data.title, { lower: true })}/`;
       }
