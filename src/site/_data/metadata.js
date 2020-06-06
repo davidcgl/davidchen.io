3238dbdc9c26bfe29caa0c880f5d30fdadf2ca6d
David Chen <cgl.david@gmail.com>
2020-06-06 16:24:36 -0700

    Add Open Graph and Twitter meta tags

--- src/site/_data/metadata.js
+++ src/site/_data/metadata.js
@@ -1,9 +1,11 @@
 module.exports = {
   title: 'David Chen',
   description: 'Ramblings by David Chen',
-  url: 'https://davidchen.io/',
+  url: 'https://davidchen.io',
+  logo: 'https://davidchen.io/assets/logo/logo-400x400.png',
   author: {
     name: 'David Chen',
     email: 'me@davidchen.io',
+    twitter: '@archenary', 
   },
 };
