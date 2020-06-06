c709b773630a07d250b63d6c0f691fb55b56eace
David Chen <cgl.david@gmail.com>
2020-06-05 23:22:11 -0700

    Remove prettierignore and paths.js

--- paths.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  siteDir: 'src/site',
-  siteAssetsDir: 'src/site/assets',
-  siteCssDir: 'src/site/assets/css',
-  siteJsDir: 'src/site/assets/js',
-  siteDataDir: 'src/site/_data',
-  manifestFile: 'src/site/_data/manifest.json',
-};
