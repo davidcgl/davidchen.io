a76585ae798eb0832d75318b898705642919c2fe
David Chen <cgl.david@gmail.com>
2020-06-06 17:05:13 -0700

    davidchen.io -> davidchen-io.now.sh

--- src/site/_data/metadata.js
+++ src/site/_data/metadata.js
@@ -1,8 +1,8 @@
 module.exports = {
   title: 'David Chen',
   description: 'Ramblings by David Chen',
-  url: 'https://davidchen.io',
-  logo: 'https://davidchen.io/assets/logo/logo-400x400.png',
+  url: 'https://davidchen-io.now.sh',
+  logo: 'https://davidchen-io.now.sh/assets/logo/logo-400x400.png',
   author: {
     name: 'David Chen',
     email: 'me@davidchen.io',
